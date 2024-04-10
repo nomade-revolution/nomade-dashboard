7b4ecfeaa19d061eeca7edc0bf32c71951df803b
Víctor Andújar <v.andujar@fruntera.com>
2024-04-10 11:25:50 +0200

    Create offersUtils - offersSections & offersTimetables

---
+++ src/sections/offers/utils/offersSections.ts
@@ -0,0 +1,12 @@
+import { HeaderSection } from "../../shared/interfaces/interfaces";
+
+export const offersHeaderSections: HeaderSection[] = [
+  { id: 1, name: "Imagen", property: "image", sortTag: "" },
+  { id: 2, name: "Cliente", property: "client", sortTag: "" },
+  { id: 3, name: "Condiciones", property: "conditions", sortTag: "" },
+  { id: 4, name: "Horario", property: "time", sortTag: "" },
+  { id: 5, name: "Max. personas", property: "max_number", sortTag: "" },
+  { id: 6, name: "Prioridad", property: "priority", sortTag: "" },
+  { id: 6, name: "Reservas", property: "reservations", sortTag: "" },
+  { id: 6, name: "Estado", property: "state", sortTag: "" },
+];
