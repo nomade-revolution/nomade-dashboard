d5339a7d8d9de833f14938a727513e66bd71a1d1
Víctor Andújar <v.andujar@fruntera.com>
2024-04-10 08:55:13 +0200

    Add customers domain at customers module

---
+++ src/modules/customers/domain/Customers.ts
@@ -0,0 +1,10 @@
+export interface Customer {
+  id: number;
+  image: string;
+  name: string;
+  category: string;
+  subcatgories: string[];
+  tags: string[];
+  description: string;
+  location: string;
+}
