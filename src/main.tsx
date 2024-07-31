ff86798a735fe8c844026ad79aed6f6eb7ea3bd3
victorandujar <victorandujarmurcia@gmail.com>
2024-07-31 20:13:02 +0200

    Add context provider with its repo to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,22 +7,25 @@ import theme from "./assets/styles/theme.ts";
 import { ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
-import { UserContextProvider } from "./sections/user/UserContext/UserContext.tsx";
+import { AuthContextProvider } from "./sections/auth/AuthContext/AuthContext.tsx";
 import { repositories } from "sections/shared/utils/repositories/repositories.ts";
 import { OffersContextProvider } from "sections/offers/OffersContext/OffersContext.tsx";
 import { CollabsContextProvider } from "sections/collabs/CollabsContext/CollabsContext.tsx";
+import { UserContextProvider } from "sections/user/UserContext/UserContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserContextProvider repository={repositories.user}>
+    <AuthContextProvider repository={repositories.user}>
       <OffersContextProvider repository={repositories.offers}>
         <CollabsContextProvider repository={repositories.collabs}>
-          <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <RouterProvider router={router} />
-          </ThemeProvider>
+          <UserContextProvider repository={repositories.users}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyles />
+              <RouterProvider router={router} />
+            </ThemeProvider>
+          </UserContextProvider>
         </CollabsContextProvider>
       </OffersContextProvider>
-    </UserContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 );
