87d1d4e9bc6146e3072c2a7131bf588a7f34f2e3
victorandujar <victorandujarmurcia@gmail.com>
2024-08-03 17:45:43 +0200

    Add functionality to delete collab at collabs repo

--- src/modules/collabs/application/routes.ts
+++ src/modules/collabs/application/routes.ts
@@ -1,3 +1,3 @@
 import environments from "@environments";
 
-export const GET_COLLABS = `${environments.API_PUBLIC_URL}/colabs`;
+export const COLLABS_BASE = `${environments.API_PUBLIC_URL}/colabs`;
