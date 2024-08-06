85aec37be3348fed5928da316fd6042e21d8970a
victorandujar <victorandujarmurcia@gmail.com>
2024-08-06 12:10:36 +0200

    Add structure + function at company module to get company data

--- src/modules/company/domain/CompanyRepository.ts
+++ src/modules/company/domain/CompanyRepository.ts
@@ -2,4 +2,5 @@ import { HttpResponseInterface } from "@core";
 
 export interface CompanyRepository<I> {
   deleteCompany(company_id: number): Promise<HttpResponseInterface<I>>;
+  getCompanyById(influencer_id: number): Promise<HttpResponseInterface<I>>;
 }
