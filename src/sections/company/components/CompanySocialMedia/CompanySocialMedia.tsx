11193037d2c5b96c8f0c1310a85b9ebe7a1c716b
victorandujar <victorandujarmurcia@gmail.com>
2024-08-06 12:16:45 +0200

    Add CompanySocialMedia compo + functionality + styles

---
+++ src/sections/company/components/CompanySocialMedia/CompanySocialMedia.tsx
@@ -0,0 +1,26 @@
+import { SocialMedia } from "@influencer/domain/InfluencerSocialMedia";
+import CompanySocialMediaStyled from "./CompanySocialMediaStyled";
+import getSocialMediaIcons from "sections/shared/utils/getSocialMediaIcons/getSocialMediaIcons";
+import { Link } from "react-router-dom";
+
+interface Props {
+  socialMedia: SocialMedia[];
+}
+
+const CompanySocialMedia = ({ socialMedia }: Props): React.ReactElement => {
+  return (
+    <CompanySocialMediaStyled className="social-media">
+      {socialMedia?.map((media) => (
+        <Link to={media.url} target="_blank">
+          <li className="social-media__data">
+            {getSocialMediaIcons(media.name)}
+            <span>{media.account_name}</span>
+            <span>{media.followers}</span>
+          </li>
+        </Link>
+      ))}
+    </CompanySocialMediaStyled>
+  );
+};
+
+export default CompanySocialMedia;
