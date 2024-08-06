b240d1f41ab1c419bc6b3090e2819b1e72be5df9
victorandujar <victorandujarmurcia@gmail.com>
2024-08-06 14:26:31 +0200

    Create LeadsPage to list leads paginated

---
+++ src/sections/leads/utils/leadsSections.ts
@@ -0,0 +1,10 @@
+import { HeaderSection } from "sections/shared/interfaces/interfaces";
+
+export const leadsHeaderSection: HeaderSection[] = [
+  { id: 1, name: "Empresa", property: "company_name", sortTag: "" },
+  { id: 3, name: "Contacto", property: "contact_name", sortTag: "" },
+  { id: 4, name: "Teléfono", property: "phone", sortTag: "" },
+  { id: 5, name: "Email", property: "email", sortTag: "" },
+  { id: 6, name: "Mensaje", property: "message", sortTag: "" },
+  { id: 7, name: "Acciones", property: "actions", sortTag: "" },
+];
