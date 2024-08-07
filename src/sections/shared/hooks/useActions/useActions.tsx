bd7740fadb2f182526a50bc832f9fb8c3eb81364
victorandujar <victorandujarmurcia@gmail.com>
2024-08-07 12:16:53 +0200

    Update handle functions from useDialog + useActions - collabs

--- src/sections/shared/hooks/useActions/useActions.tsx
+++ src/sections/shared/hooks/useActions/useActions.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import * as collabStates from "../../../collabs/utils/collabsStates";
+import { useCollabsContext } from "sections/collabs/CollabsContext/useCollabsContext";
+
 const useActions = () => {
+  const { updateCollabState } = useCollabsContext();
+  const navigate = useNavigate();
+
   const handleIsDialogOpen = (setter: (value: boolean) => void) => {
     setter(true);
   };
@@ -6,9 +13,30 @@ const useActions = () => {
   const handleIsDialogClosed = (setter: (value: boolean) => void) => {
     setter(false);
   };
+
+  const acceptCollab = async (collabId: number) => {
+    await updateCollabState(collabId, collabStates.COLAB_PENDING_COMPANY_STATE);
+    setTimeout(() => navigate(0), 1500);
+  };
+
+  const rejectCollab = async (
+    collabId: number,
+    rejected_collab_reason_id: number,
+  ) => {
+    await updateCollabState(
+      collabId,
+      collabStates.COLAB_REJECTED_STATE,
+      rejected_collab_reason_id,
+    );
+
+    setTimeout(() => navigate(0), 1500);
+  };
+
   return {
     handleIsDialogOpen,
     handleIsDialogClosed,
+    acceptCollab,
+    rejectCollab,
   };
 };
 
