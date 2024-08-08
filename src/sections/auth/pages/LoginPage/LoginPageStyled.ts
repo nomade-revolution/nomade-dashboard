54810a47c2d1fdc648c1f423c56ec7b5c73ca66f
victorandujar <victorandujarmurcia@gmail.com>
2024-08-08 16:20:36 +0200

    Refactor login screen & create NomadeLogoSection

--- src/sections/auth/pages/LoginPage/LoginPageStyled.ts
+++ src/sections/auth/pages/LoginPage/LoginPageStyled.ts
@@ -32,49 +32,6 @@ const LoginPageStyled = styled.main`
       align-items: center;
       gap: 30px;
     }
-
-    &__circles-container {
-      position: relative;
-      width: 100%;
-      height: 50%;
-    }
-
-    &__big-circle,
-    &__small-circle {
-      position: absolute;
-      background: ${(props) => props.theme.colors.mainColor};
-      border-radius: 50%;
-      opacity: 0.3;
-    }
-
-    &__big-circle {
-      width: 30rem;
-      height: 30rem;
-      top: 9%;
-      left: 20%;
-
-      @media (max-width: 1600px) {
-        width: 20rem;
-        height: 20rem;
-        left: 12%;
-        top: 0;
-      }
-    }
-
-    &__small-circle {
-      width: 14rem;
-      height: 14rem;
-      top: 57%;
-      left: 52%;
-      z-index: 1;
-
-      @media (max-width: 1600px) {
-        width: 12rem;
-        height: 12rem;
-        left: 48%;
-        top: 47%;
-      }
-    }
   }
 `;
 
