8ff67d8fe048331b0b8273c36e876cf955edbc0d
victorandujar <victorandujarmurcia@gmail.com>
2024-08-08 16:22:04 +0200

    Add LeadsForm compo + add its main styles

---
+++ src/sections/leads/components/LeadsForm/LeadsFormStyled.ts
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+
+const LeadsFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: flex-start;
+  width: 100%;
+  background: ${(props) => props.theme.colors.light};
+  padding: 30px;
+  border-radius: ${(props) => props.theme.borderRadius.badges};
+  box-shadow: 0px 0px 20px 0.2em rgba(0, 0, 0, 0.1);
+
+  .datasheet-form {
+    &__section {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      gap: 20px;
+
+      @media (min-width: 1000px) and (max-width: 1600px) {
+        width: 80%;
+        flex-direction: row;
+      }
+
+      @media (min-width: 1600px) {
+        gap: 40px;
+        flex-direction: row;
+      }
+    }
+
+    &__submit {
+      background: ${(props) => props.theme.colors.mainColor};
+      color: ${(props) => props.theme.fontsColors.light};
+      font-weight: 700;
+      padding: 10px 20px;
+      width: 100%;
+      border-radius: ${(props) => props.theme.borderRadius.submitButton};
+
+      @media (min-width: 1000px) {
+        width: ${(props) => props.theme.widths.datasheetLargeInputs};
+      }
+    }
+  }
+
+  .form-subsection {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    &__label {
+      font-size: small;
+      color: ${(props) => props.theme.fontsColors.lightGrey};
+    }
+
+    &__field,
+    &__field-large,
+    &__field-textarea {
+      display: flex;
+      align-items: center;
+      height: 40px;
+      border-radius: ${(props) => props.theme.borderRadius.inputs};
+      border: 1px solid ${(props) => props.theme.borders.lightGrey};
+      padding: 0 8px;
+      width: 100%;
+      gap: 40px;
+
+      @media (min-width: 1000px) and (max-width: 1600px) {
+        width: ${(props) => props.theme.widths.datasheetSmallestInputs};
+      }
+
+      @media (min-width: 1600px) {
+        width: ${(props) => props.theme.widths.datasheetSmallerInputs};
+      }
+    }
+
+    &__field-large,
+    &__field-textarea {
+      width: 100%;
+      @media (min-width: 1000px) {
+        width: ${(props) => props.theme.widths.datasheetSmallerInputs};
+      }
+
+      @media (min-width: 1200px) {
+        width: ${(props) => props.theme.widths.datasheetSmallInputs};
+      }
+
+      @media (min-width: 1600px) {
+        width: ${(props) => props.theme.widths.datasheetLargeInputs};
+      }
+    }
+    &__taxes-item {
+      font-size: ${(props) => props.theme.fontsSize.__SMM};
+    }
+
+    &__checkbox {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: left;
+      gap: 20px;
+    }
+
+    &__field-textarea {
+      height: 200px;
+      padding: 10px;
+      font-family: inherit;
+    }
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+
+    resize: none;
+  }
+`;
+
+export default LeadsFormStyled;
