ebb087195da7dfc0216ca2ed19e06e0d68b2690d
victorandujar <victorandujarmurcia@gmail.com>
2024-08-08 16:23:05 +0200

    Add LeadsSubmitInfoPage + add its styles

---
+++ src/sections/leads/pages/LeadsSubmitInfoPage/LeadsSubmitInfoPageStyled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const LeadsSubmitInfoPageStyled = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  .leadSubmit-page {
+    height: 100%;
+
+    &__company {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 40px;
+      background: ${(props) => props.theme.colors.secondaryColor};
+      height: 100vh;
+      width: 50%;
+    }
+
+    &__slogan {
+      color: ${(props) => props.theme.colors.mainColor};
+    }
+
+    &__form-section {
+      width: 50%;
+      padding: 40px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 30px;
+    }
+  }
+`;
+
+export default LeadsSubmitInfoPageStyled;
