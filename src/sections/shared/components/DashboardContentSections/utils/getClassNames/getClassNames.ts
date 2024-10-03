8c7b4becd50d9da4eb12bcd7f0844b4aa9030c0c
victorandujar <victorandujarmurcia@gmail.com>
2024-10-03 11:30:58 +0200

    Add getCollabsStates func & getCollabStateClassname func

--- src/sections/shared/components/DashboardContentSections/utils/getClassNames/getClassNames.ts
+++ src/sections/shared/components/DashboardContentSections/utils/getClassNames/getClassNames.ts
@@ -1,5 +1,11 @@
 import { CollabTypes, FullCollab } from "modules/collabs/domain/Collabs";
 import { User, UserTypes } from "modules/user/domain/User";
+import {
+  COLAB_ACCEPTED_STATE,
+  COLAB_CANCELLED_STATE,
+  COLAB_REJECTED_STATE,
+  COLAB_SENT_STATE,
+} from "sections/collabs/utils/collabsStates";
 
 export const getTypesClassNames = (section: object, className: string) => {
   return `${className}__type-section ${
@@ -20,5 +26,22 @@ export const getTypesClassNames = (section: object, className: string) => {
                   : (section as FullCollab).type === CollabTypes.activity
                     ? `${className}__type-section--activity`
                     : ""
+  }`;
+};
+
+export const getCollabStateClassname = (
+  state_id: number,
+  className: string,
+) => {
+  return `${className}__state-section ${
+    state_id === COLAB_ACCEPTED_STATE
+      ? `${className}__state-section--accepted`
+      : state_id === COLAB_REJECTED_STATE
+        ? `${className}__state-section--rejected`
+        : state_id === COLAB_CANCELLED_STATE
+          ? `${className}__state-section--cancelled`
+          : state_id === COLAB_SENT_STATE
+            ? `${className}__state-section--sent`
+            : ""
   }`;
 };
