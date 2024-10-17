d3672e650713a164100aaab811a67a0fbdfc4ec9
victorandujar <victorandujarmurcia@gmail.com>
2024-10-17 12:20:22 +0200

    Add PlansPage + add its styles

---
+++ src/sections/plans/pages/PlansPage/PlansPage.tsx
@@ -0,0 +1,35 @@
+import ReusablePageStyled from "assets/styles/ReusablePageStyled";
+import { useEffect } from "react";
+import { usePlansContext } from "sections/plans/PlansContext/usePlansContext";
+import { monthPlansTableSections } from "sections/plans/utils/plansTableSections";
+import DashboardTable from "sections/shared/components/DashboardTable/DashboardTable";
+import ReusableTabSelector from "sections/shared/components/ReusableTabSelector/ReusableTabSelector";
+
+const PlansPage = (): React.ReactElement => {
+  const tabs = ["Mensual", "Trimestral"];
+
+  const { getPlans, plans } = usePlansContext();
+
+  useEffect(() => {
+    getPlans();
+  }, [getPlans]);
+
+  const monthPlans = plans.filter((items) => items.billing === "Mensual");
+
+  return (
+    <ReusablePageStyled>
+      <ReusableTabSelector
+        tabs={tabs}
+        content={[
+          <DashboardTable
+            bodySections={monthPlans}
+            headerSections={monthPlansTableSections}
+            pageName=""
+          />,
+        ]}
+      />
+    </ReusablePageStyled>
+  );
+};
+
+export default PlansPage;
---
+++ src/sections/plans/pages/PlansPage/PlansPageStyled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const PlansPageStyled = styled.main``;
+
+export default PlansPageStyled;
